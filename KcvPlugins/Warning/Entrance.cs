6501d49af1e140c93b2973e912b29747da9caf5d
amming <y2443@163.com>
2015-01-29 23:42:26 +0800

    add warning project

---
+++ KcvPlugins/Warning/Entrance.cs
@@ -0,0 +1,72 @@
+﻿using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models.Raw;
+using MetroRadiance;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using kcv = Grabacr07.KanColleViewer;
+
+namespace Warning
+{
+    [Export(typeof(IToolPlugin))]
+    [ExportMetadata("Title", "Warning")]
+    [ExportMetadata("Description", "Feet Hp Warning")]
+    [ExportMetadata("Version", Entrance.IToolPluginVersion)]
+    [ExportMetadata("Author", "@AMing")]
+    public class Entrance : IToolPlugin
+    {
+        private readonly ViewModels.SettingsViewModel settingsViewModel = new ViewModels.SettingsViewModel();
+        private readonly ViewModels.FleetsViewModel fleetsViewModel = new ViewModels.FleetsViewModel();
+
+
+        public const string IToolPluginVersion = "1.0";
+        public string ToolName
+        {
+            get
+            {
+                return "Warning";
+                //return TextResource.Plugin_ToolName;
+            }
+        }
+
+        public object GetToolView()
+        {
+            return new Views.SettingsControl { DataContext = this.settingsViewModel };
+        }
+
+        public object GetSettingsView()
+        {
+            return null;
+        }
+
+        public Entrance()
+        {
+            Init();
+        }
+
+        ~Entrance()
+        {
+            Exit();
+        }
+
+        private void Init()
+        {
+            //Modules.InitModules.Current.Initialize();
+            //Data.Settings.Load();
+
+
+        }
+
+        private void Exit()
+        {
+            //Data.Settings.Current.Save();
+        }
+    }
+   
+}
---
+++ KcvPlugins/Warning/ViewModels/FleetsViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Models;
+using Grabacr07.KanColleWrapper;
+using Livet;
+using Livet.EventListeners;
+using Grabacr07.KanColleViewer.ViewModels;
+using Grabacr07.KanColleViewer.ViewModels.Contents.Fleets;
+
+namespace Warning.ViewModels
+{
+    public class FleetsViewModel : TabItemViewModel
+    {
+        public override string Name
+        {
+            get { return Grabacr07.KanColleViewer.Properties.Resources.Fleets; }
+            protected set { throw new NotImplementedException(); }
+        }
+
+        #region Fleets 変更通知プロパティ
+
+        private FleetViewModel[] _Fleets;
+
+        public FleetViewModel[] Fleets
+        {
+            get { return this._Fleets; }
+            set
+            {
+                if (this._Fleets != value)
+                {
+                    this._Fleets = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region SelectedFleet 変更通知プロパティ
+
+        private FleetViewModel _SelectedFleet;
+
+        /// <summary>
+        /// 現在選択されている艦隊を取得または設定します。
+        /// </summary>
+        public FleetViewModel SelectedFleet
+        {
+            get { return this._SelectedFleet; }
+            set
+            {
+                if (this._SelectedFleet != value)
+                {
+                    if (this._SelectedFleet != null) this.SelectedFleet.IsSelected = false;
+                    if (value != null) value.IsSelected = true;
+                    this._SelectedFleet = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        public bool IsSupportedNotification
+        {
+            get { return Helper.IsWindows8OrGreater; }
+        }
+
+
+        public bool IsInit { get; set; }
+        public void Initialize()
+        {
+            try
+            {
+                this.CompositeDisposable.Add(new PropertyChangedEventListener(KanColleClient.Current.Homeport.Organization)
+			    {
+				    { "Fleets", (sender, args) => this.UpdateFleets() },
+			    });
+                this.UpdateFleets();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void UpdateFleets()
+        {
+            this.Fleets = KanColleClient.Current.Homeport.Organization.Fleets.Select(kvp => new FleetViewModel(kvp.Value)).ToArray();
+            this.SelectedFleet = this.Fleets.FirstOrDefault();
+        }
+    }
+}
